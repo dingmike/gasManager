1ed826f4939a87ff3496d24d78ab78ffeb96ab59
admin <2252277509@qq.com>
2019-01-07 08:57:10 +0800

    11

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,5 +1,7 @@
 module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
-  BASE_API: '"https://api-dev"'
+  // BASE_API: '"http://192.168.10.61"'
+  BASE_API: '"http://192.168.10.23:9100"'
+  // BASE_API: '"https://api-dev"'
 }
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,7 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
-  BASE_API: '"https://api-prod"'
+  // BASE_API: '"http://192.168.10.61"'
+  BASE_API: '"http://192.168.10.23:9100"'
+  // BASE_API: '"https://api-prod"'
 }
